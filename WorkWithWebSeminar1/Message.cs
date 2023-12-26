7d53c92db644e9d86460d9fd77ebb2820855bd5d
McAr0v <dvijkz@gmail.com>
2023-12-26 21:08:54 +0600

    Добавьте файлы проекта.

---
+++ WorkWithWebSeminar1/Message.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+
+namespace WorkWithWebSeminar1
+{
+    internal class Message
+    {
+        public string Name { get; set; }
+
+        public string Text { get; set; }
+
+        public DateTime Stime { get; set; }
+
+        public string ToJson()
+        {
+            return JsonSerializer.Serialize(this);
+        }
+
+        public static Message? FromJson(string message)
+        {
+            return JsonSerializer.Deserialize<Message>(message);
+        }
+
+        public Message (string nik, string text)
+        {
+            this.Name = nik;
+            this.Text = text;
+            this.Stime = DateTime.Now;
+        }
+
+        public Message () { }
+
+        public override string ToString()
+        {
+            //return $"Nickname:{this.Name}, Text:{this.Text}, Date:{this.date}";
+            return $"Получено сообщение от {Name} ({Stime}): \n {Text}";
+        }
+    }
+}
